42a93d316369b88f1b0faadd49c88961fd7ff3e4
Ana Isabel Portal Diaz <anportal@uclv.cu>
2023-06-14 19:46:45 -0400

    En SBC arreglada la ortografia

--- src/sbc_for_hipertension_tratament_japp/SBC.pl
+++ src/sbc_for_hipertension_tratament_japp/SBC.pl
@@ -1,6 +1,6 @@
-% Clasificacion de la PA
-% PAS presion arterial sistolica
-% PAD presion arterial diastolica
+% Clasificación de la PA
+% PAS presión arterial sistólica
+% PAD presión arterial diastólica
 categoria(PAS,PAD,'Hipertensión sistólica aislada'):-
   PAS>=140,PAD<90,!.
 categoria(PAS,PAD,'Grado III'):-
@@ -16,11 +16,12 @@ categoria(PAS,PAD,'Prehipertensión'):-
   PAS>=120,!;
   PAD>=80,!.
 categoria(_,_,'Normal').
+
 % FAC factores de riesgo cardiovasculares
-% LOD lesion en organo diana
+% LOD lesión en órgano diana
 % DM diabetis mellitus
 % RCV riesgo cardiovascular
-% Reglas para la estratificacion del RCV
+% Reglas para la estratificación del RCV
 rcv(Lista_FRC,Categoria,Riesgo):-
   length(Lista_FRC,Y),
   (
@@ -41,14 +42,16 @@ rcv(Lista_FRC,Categoria,Riesgo):-
     (Categoria='Grado I';Categoria='Grado II';Categoria='Grado III'),Riesgo='Riesgo alto'
    )
   ).
-% Tipos de estrategias terapeuticas
+  
+% Tipos de estrategias terapéuticas
 estrategia(1,'Sugerir cambios en el estilo de vida. No intervenir sobre la PA.').
 estrategia(2,'Cambios en el estilo de vida. No intervenir sobre la PA.').
 estrategia(3,'Cambios en el estilo de vida. No intervenir sobre la PA. Considerar tratamiento de LOD.').
 estrategia(4,'Cambios en el estilo de vida durante varios meses, si no control añadir tratamiento para PA con un objetivo de <140/90.').
 estrategia(5,'Cambios en el estilo de vida durante varias semanas, si no control añadir tratamiento para PA con un objetivo de <140/90.').
 estrategia(6,'Cambios en el estilo de vida. Tratamiento inmediato para la PA con un objetivo de <140/90. Tratamiento de FRC.').
-% Propuesta de estrategia terapeutica segun riesgo cardiovascular
+
+% Propuesta de estrategia terapéutica según riesgo cardiovascular
 propuesta_estrategia_terapeutica(Lista_FRC,Categoria,Estrategia):-
   length(Lista_FRC,Y),
   (
@@ -71,8 +74,9 @@ propuesta_estrategia_terapeutica(Lista_FRC,Categoria,Estrategia):-
         (Categoria='Grado I';Categoria='Grado II';Categoria='Grado III'),estrategia(6,Estrategia)
     )
   ).
-%Contraindicaciones absolutas por farmacos
-%contraind_abso_por_f(L:Lista de enfermedades a las que esta contraindicado,F: Farmaco).
+  
+%Contraindicaciones absolutas por fármacos
+%contraind_abso_por_f(L:Lista de enfermedades a las que esta contraindicado,F: Fármaco).
 contraind_abso_por_f(['Gota'],'Diuréticos tiazídicos').
 contraind_abso_por_f(['Asma','Bloqueo AV grado 2','Bloqueo AV grado 3'],'Betabloqueadores').
 contraind_abso_por_f(['Bloqueo AV grado 2','Bloqueo AV grado 3','Bloqueo trifascicular',
@@ -82,9 +86,10 @@ contraind_abso_por_f(['Bloqueo AV grado 2','Bloqueo AV grado 3','Bloqueo trifasc
 contraind_abso_por_f(['Embarazo', 'Angioedema', 'Hiperpotasemia', 'Estenosis arterial renal bilateral'],'IECA').
 contraind_abso_por_f(['Embarazo', 'Hiperpotasemia', 'Estenosis arterial renal bilateral'],'ARA II').
 contraind_abso_por_f(['Insuficiencia renal aguda', 'Insuficiencia renal grave', 'Hiperpotasemia'],'Antagonistas del receptor mineralcorticoideo').
+
 %Contraindicaciones absolutas
 %contraindicacion_a(ListaDeSintomas,Farmaco) retorna verdadero(esta contraindicado)
-%en caso de que alguno de los sintomas este contraindicado para el tipo de farmaco
+%en caso de que alguna de las condiciones médicas tenga contraindicado el uso del fármaco
 % Ejemplo:
 %?- contraind_a(['Gota','Asma','Bloqueo AV grado 2','Bloqueo AV grado 3'],F).
 %F = 'Diuréticos tiazídicos' ;
@@ -92,7 +97,9 @@ contraind_abso_por_f(['Insuficiencia renal aguda', 'Insuficiencia renal grave', 
 %F = 'Antagonistas del calcio-verapamilo' ;
 %F = 'Antagonistas del calcio-dialtiazem' ;
 %false.
+
 contraind_a(ListaDeSintomas,F):- contraind_abso_por_f(LCpF,F),not(disjuntos(LCpF,ListaDeSintomas)).
+
 %Disjuncion entre conjuntos A y B
 disjuntos([],_).
 disjuntos([X|Ra],B):- memb(X,B),
@@ -101,10 +108,12 @@ disjuntos([X|Ra],B):- memb(X,B),
 memb(_, []) :- fail.
 memb(X,[X|_R]).
 memb(X,[_H|C]):- memb(X,C).
-% Devolver las contraindicaciones en una lista
+
+% Devolver las contraindicaciones absolutas en una lista
 contraindicaciones_absolutas(ListaFRC,L_farm):- setof(F,contraind_a(ListaFRC,F),L_farm).
 
-% Contraindicaciones relativas por farmacos
+
+% Contraindicaciones relativas por fármacos
 %contraind_rela_por_f(L:Lista de enfermedades a las que esta contraindicado,F: Farmaco).
 contraind_rela_por_f(['Síndrome metabólico','Intolerancia a la glucosa','Embarazo','Hiperpotasemia',
                        'Hipopotasemia','Atletas en activo'],'Diuréticos tiazídicos').
@@ -116,4 +125,5 @@ contraind_rela_por_f(['Mujeres en edad fértil'],'ARA II').
 %Contraindicaciones relativas
 contraind_r(ListaDeSintomas,F):- contraind_rela_por_f(LCpF,F),not(disjuntos(LCpF,ListaDeSintomas)).
 
+% Devolver las contraindicaciones relativas en una lista
 contraindicaciones_relativas(ListaFRC,L_farm):- setof(F,contraind_r(ListaFRC,F),L_farm).
